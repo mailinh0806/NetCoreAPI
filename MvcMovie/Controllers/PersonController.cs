d400d58d518b5621b8891bcd5542262205ad9535
mailinh0806 <vumailinh29122002@gmail.com>
2024-06-13 16:43:36 +0700

    phantrang

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -1,11 +1,13 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using MvcMovie.Data;
 using MvcMovie.Models;
 using MvcMovie.Models.Process;
 using OfficeOpenXml;
+using X.PagedList;
 
 namespace MvcMovie.Controllers
 {
@@ -17,11 +19,23 @@ namespace MvcMovie.Controllers
     {
         _context = context;
     }
-    public async Task<IActionResult> Index()
-    {
-        var model = await _context.Person.ToListAsync();
-        return View(model);
-    }
+    public async Task<IActionResult> Index(int? page, int? PageSize)
+        {
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+                new SelectListItem() { Value="3", Text= "3"},
+                new SelectListItem() { Value="5", Text= "5"},
+                new SelectListItem() { Value="10", Text= "10"},
+                new SelectListItem() { Value="15", Text= "15"},
+                new SelectListItem() { Value="25", Text= "25"},
+                new SelectListItem() { Value="50", Text= "50"},
+            };
+            int pagesize = (PageSize ?? 3);
+            ViewBag.psize = pagesize;
+            var model = _context.Person.ToList().ToPagedList(page ?? 1, pagesize);
+            return View(model);
+        }
+    
     public IActionResult Create()
     {
         return View();
